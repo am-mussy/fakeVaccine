a9902343507543260faabbee004ec05a77bd3cca
am.mussy <aydoganwork@gmail.com>
2022-01-30 13:51:46 +0300

    New world!

---
+++ fake-vaccine/src/Vaccine/Vaccine.jsx
@@ -0,0 +1,19 @@
+
+
+const Header = () => 
+    <div>
+        <div>
+            <a href="https://www.gosuslugi.ru/">ГОСУСЛУГИ</a>
+        </div>
+    </div>
+  
+
+
+const Vaccine = () => 
+    <div>
+        <Header/>
+    </div>
+
+
+
+export default Vaccine
